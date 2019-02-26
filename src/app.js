ad740896b0caf098c604efc387d2e12df4c894ee
Kikoken <kikoken@gmail.com>
2019-02-26 09:39:30 -0300

    update route

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express()
 const PORT = process.env.PORT || 8000
 
 app.use(bodyParser.json())
-app.use('/', WheatherRoute)
+app.use('/api', WheatherRoute)
 
 app.server = app.listen(PORT, () => {
   console.log(`API running in port ${PORT}`)
