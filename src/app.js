014dcf63acdf1e1d115e9975f8a84d6146332bc6
Kikoken <kikoken@gmail.com>
2019-02-27 01:50:39 -0300

    added cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 import {
   WheatherRoute
@@ -8,6 +9,7 @@ import {
 const app = express()
 const PORT = 8000
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use('/api', WheatherRoute)
 
