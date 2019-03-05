25a42e68bc0fa953add7e888c8658d1ac120ac21
Kikoken <kikoken@gmail.com>
2019-03-05 09:44:29 -0300

    update format capital

--- src/api/capital.api.js
+++ src/api/capital.api.js
@@ -7,7 +7,7 @@ class CapitalApi {
   async getCoordByName(country) {
     let responseCountry = await fetch(`${this.uri}name/${country}`)
     let dataCountry = await responseCountry.json()
-    let responseCapital = await fetch(`${this.uri}capital/${dataCountry[0].capital}`)
+    let responseCapital = await fetch(`${this.uri}capital/${String(dataCountry[0].capital).normalize('NFD').replace(/[\u0300-\u036f]/g, "")}`)
     
     let data = await responseCapital.json()
     return {name: dataCountry[0].capital , coord: data[0].latlng.join(',')}
